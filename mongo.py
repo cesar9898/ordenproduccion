e4cde15bf635c24392d9bfa7d27ca4a6de48f0a7
cesar9898 <graficacevallos@gmail.com>
2025-05-30 09:52:42 +0000

    creacion de base de datos mongo

---
+++ mongo.py
@@ -0,0 +1,27 @@
+import collections
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/appordenproduccion")
+
+db = client.ordenesproduccion
+coleccion = db.ordenes
+
+lista = [
+    {"razon_social": "indumaster", "ruc": "1390140858001", "telefono": "0987107295",
+     "descripcion_trabajo": "flayers", "fecha_entrega": "2025-10-01",
+     "cantidad": 5000, "precio_unitario": 0.5, "total": 500},   
+    {"razon_social": "epespo", "ruc": "1709271514001", "telefono": "0936148523",
+     "descripcion_trabajo": "rollup", "fecha_entrega": "2025-10-05",
+     "cantidad": 5, "precio_unitario": 75, "total": 375},
+    {"razon_social": "rav", "ruc": "157894269001", "telefono": "052689352",
+     "descripcion_trabajo": "revistas", "fecha_entrega": "2025-10-05",
+     "cantidad": 5, "precio_unitario": 75, "total": 375}
+     {"razon_social": "servicios_veep", "ruc": "1234567890123", "telefono": "0987654321",
+      "descripcion_trabajo": "block_formulario", "fecha_entrega": "2025-10-10",
+      "cantidad": 20, "precio_unitario": 15, "total": 300}                                               
+]
+
+collections - db.list_collection_names()
+print(collections)
+
+
