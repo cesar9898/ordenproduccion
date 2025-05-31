b80ff1b2ee12ff5fd6b655971385a8a5451afe4d
cesar9898 <graficacevallos@gmail.com>
2025-05-31 03:53:12 +0000

    creacionde base de datos sqllite y ensercion de datos

---
+++ sqllite.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+with sqlite3.connect('app.db') as con:
+    cursor = con.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS orden_produccion (
+            ruc TEXT PRIMARY KEY,
+            fecha TEXT,
+            razon_social TEXT,
+            descripcion_trabajo TEXT,
+            cantidad INTEGER,
+            precio_total REAL,
+            estado TEXT
+        )
+        """
+    )
+    cursor.executescript(
+        """
+        INSERT INTO orden_produccion (ruc, fecha, razon_social, descripcion_trabajo, cantidad, precio_total, estado)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        """,
+        ('1234567890', '2023-10-01', 'La favorita', 'revistas', 100, 1500.00, 'Pendiente')
+    )
+
+print("salir")
